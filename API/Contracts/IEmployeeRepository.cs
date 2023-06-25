856ebffe585f4e5ed6900a45c66949302f730d05
Shandika Galih <shandika2001@gmail.com>
2023-06-25 16:56:47 +0700

    update

--- API/Contracts/IEmployeeRepository.cs
+++ API/Contracts/IEmployeeRepository.cs
@@ -1,12 +1,6 @@
 ﻿using API.Models;
-
 namespace API.Contracts;
 
-public interface IEmployeeRepository
+public interface IEmployeeRepository : IGeneralRepository<Employee>
 {
-    ICollection<Employee> GetAll();
-    Employee? GetByGuid(Guid guid);
-    Employee Create(Employee employee);
-    bool Update(Employee employee);
-    bool Delete(Guid guid);
 }
